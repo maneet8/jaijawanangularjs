365ee728e72a15082eb42d866033b7c6591bba72
Maneet Kumar <maneetkum@gmail.com>
2018-02-15 09:56:50 +0530

    test-form

--- assets/js/mainController.js
+++ assets/js/mainController.js
@@ -140,6 +140,20 @@ app.config(function ($routeProvider) {
         });
 });
 
+// app.controller("dealerCtrl", function ($scope, $sce) {
+//     $scope.data = [
+//         {
+//             "name": "Biodiesel Retail Outlet",
+//             "imgpath": "./assets/img/angularjslogo.jpg",
+//             "topic": "Biodiesel Retail Outlet",
+//             "Description": "Fill this Application Form if you want to open a Retail Outlet",
+//             "id": "1",
+//             "url": "https://docs.google.com/forms/d/e/1FAIpQLSeztynG2BBTWrhwVXtEEcFHWabEIMdk8SMPVnstQIz-wesMYA/viewform?embedded=true"
+//         }
+//     ]
+//     $scope.name = "John Doe";
+// });
+
 app.filter('trusted', ['$sce', function ($sce) {
     return function (url) {
         return $sce.trustAsResourceUrl(url);
