4c3d19a88a34dabf6cf5d012ad26d6e69a944c74
Maneet Kumar <maneetkum@gmail.com>
2018-03-14 01:46:30 +0530

    addition of delears

--- assets/js/dealersController.js
+++ assets/js/dealersController.js
@@ -2,14 +2,53 @@
 app.controller("dealerCtrl", function ($scope, $sce) {
     $scope.data = [
         {
-            "name": "Biodiesel Retail Outlet",
-            "imgpath": "./assets/img/angularjslogo.jpg",
-            "topic": "Biodiesel Retail Outlet",
-            "Description": "Fill this Application Form if you want to open a Retail Outlet",
+            "name": "Mr.  Dinesh Kumar Shivhare",
+            "imgpath": "./assets/img/dinesh.jpg",
+            "topic": "Angular JS",
+            "address": "Village – Churkhi    Distt - Jalaun  U.P 285203",
             "id": "1",
-            "url": "https://docs.google.com/forms/d/e/1FAIpQLSeztynG2BBTWrhwVXtEEcFHWabEIMdk8SMPVnstQIz-wesMYA/viewform?embedded=true"
+            "url": "https://docs.google.com/forms/d/e/1FAIpQLSdEfI7ILuKb-DoTGfbYyc5oO9Lew0q-6ZL82TpT2oSYZH1eYw/viewform?embedded=true"
+        },
+        {
+            "name": "Mr. Malkhan S/O Bhaiyadeen",
+            "imgpath": "./assets/img/malkhan.jpg",
+            "topic": "AWS",
+            "address": "Bahauri,  Kadaura  Distt – Jalaun U.P 285203",
+            "id": "2",
+            "url": "http://localhost:9092/"
+        },
+        {
+            "name": "Mr. Ajay Kumar S/O  Gilla",
+            "imgpath": "./assets/img/ajay.jpg",
+            "topic": "Bootstrap",
+            "address": "Terahi Mafi Distt - Banda U.P 210128",
+            "id": "3",
+            "url": "http://localhost:8001/"
+        },
+        {
+            "name": "Mr. Mustak Khan S/O Taj Khan",
+            "imgpath": "./assets/img/mustak.jpg",
+            "topic": "Restful",
+            "address": "Village – Padodhri Distt- Hamirpur U.P 210507",
+            "id": "4",
+            "url": "http://localhost:8001/"
+        },
+        {
+            "name": "Mr. Jitendra Singh S/O Ramprakash",
+            "imgpath": "./assets/img/jitendra.jpg",
+            "topic": "Restful",
+            "address": "Village – Badeha , Tindwari Distt - Banda U.P 210128",
+            "id": "5",
+            "url": "http://localhost:8001/"
+        },
+        {
+            "name": "Mr. Sriram Gupta S/O Shiv Prashad Gupta",
+            "imgpath": "./assets/img/sriram.jpg",
+            "topic": "Restful",
+            "address": "Village - Oran Distt - Banda U.P 210203",
+            "id": "6",
+            "url": "http://localhost:8001/"
         }
     ]
-    $scope.name = "John Doe";
 });
 
